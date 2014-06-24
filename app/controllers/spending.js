e540540da29d4996219d5d115394c2d3f77292fb
Alex Zirbel <alex.zirbel@addepar.com>
2014-06-24 01:11:56 -0700

    Filter transactions and add analysis tab

--- app/controllers/spending.js
+++ app/controllers/spending.js
@@ -2,7 +2,24 @@ export default Ember.ArrayController.extend({
   needs: ['application'],
   numRows: 100,
 
-  transactions: Ember.computed.alias('controllers.application.model'),
+  allTransactions: Ember.computed.alias('controllers.application.model'),
+
+  transactions: function() {
+    if (this.get('selectedCategory') != null) {
+      var _this = this;
+      var filteredTransactions = _.filter(this.get('allTransactions.content'), function(transaction) {
+        return transaction.get('category') === _this.get('selectedCategory.name');
+      });
+      return _.sortBy(filteredTransactions, function (transaction) {
+        return transaction.get('date').getTime();
+      });
+    }
+    return _.sortBy(this.get('allTransactions.content'), function (transaction) {
+      return transaction.get('date').getTime();
+    });
+  }.property('allTransactions', 'selectedCategory'),
+
+  selectedCategory: null,
 
   categories: [
     {
@@ -63,5 +80,45 @@ export default Ember.ArrayController.extend({
     return [dateColumn, descriptionColumn, categoryColumn, amountColumn];
   }.property(),
 
-  content: Ember.computed.alias('controllers.application.model')
+  spendingByCategory: function() {
+    var _this = this;
+    return _.map(this.get('categories'), function(category) {
+      var transactionsInCategory = _.filter(_this.get('transactions'), function(transaction) {
+        return transaction.get('category') === category.name;
+      });
+      var total = _.reduce(transactionsInCategory, function(total, transaction) {
+        return total + transaction.get('amount');
+      }, 0);
+      return {
+        "label": category.name,
+        "value": total
+      };
+    });
+  }.property('categories', 'transactions'),
+
+  view: null,
+
+  transactionsViewIsSelected: function() {
+    return this.get('view') === 'transactions' || this.get('view') == null;
+  }.property('view'),
+  analysisViewIsSelected: function() {
+    return this.get('view') === 'analysis';
+  }.property('view'),
+  budgetsViewIsSelected: function() {
+    return this.get('view') === 'budgets';
+  }.property('view'),
+
+  actions: {
+    selectTransactionsView: function() {
+      this.set('view', 'transactions');
+    },
+
+    selectAnalysisView: function() {
+      this.set('view', 'analysis');
+    },
+
+    selectBudgetsView: function() {
+      this.set('view', 'budgets');
+    }
+  }
 });
