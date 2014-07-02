65270566ad983277d0b6dc41b89e67f585f57b16
Alex Zirbel <alex.zirbel@addepar.com>
2014-07-02 01:53:06 -0700

    Allow editing category in the transactions table
    * Still a bit buggy; overflow problems in particular

--- app/controllers/spending.js
+++ app/controllers/spending.js
@@ -5,6 +5,7 @@ export default Ember.ArrayController.extend({
   allTransactions: Ember.computed.alias('controllers.application.model'),
 
   transactions: function() {
+    console.log('recomputing the table.');
     if (this.get('selectedCategory') != null) {
       var _this = this;
       var filteredTransactions = _.filter(this.get('allTransactions.content'), function(transaction) {
@@ -17,7 +18,7 @@ export default Ember.ArrayController.extend({
     return _.sortBy(this.get('allTransactions.content'), function (transaction) {
       return transaction.get('date').getTime();
     });
-  }.property('allTransactions', 'selectedCategory'),
+  }.property('allTransactions', 'allTransactions.@each.category', 'selectedCategory'),
 
   selectedCategory: null,
 
@@ -57,14 +58,76 @@ export default Ember.ArrayController.extend({
       contentPath: 'description'
     });
     var categoryColumn = Ember.Table.ColumnDefinition.create({
+      target: this,
       isResizable: false,
       textAlign: 'text-align-left',
-      tableCellViewClass: Ember.Table.TableCell.extend({
-        templateName: 'category-table-cell',
-      }),
       columnWidth: 120,
       headerCellName: 'Category',
-      contentPath: 'category'
+      contentPath: 'category',
+      getCellContent: function(row) {
+        return row['category'];
+      },
+      setCellContent: function(row, value) {
+        console.log('setting cell content.');
+        var outerRow = _.find(
+          this.get('target').get('allTransactions.content'), function(t) {
+            return t.get('id') == row.get('id');
+          }
+        );
+        outerRow.set('category', value.name);
+      },
+      tableCellViewClass: Ember.Table.TableCell.extend({
+        isEditing: false,
+        templateName: 'category-table-cell',
+        categories: [
+          {
+            name: 'food',
+            symbol: 'fo',
+            def: 'food'
+          },
+          {
+            name: 'entertainment',
+            symbol: 'en',
+            def: 'entertainment'
+          },
+          {
+            name: 'UNCATEGORIZED',
+            symbol: 'UN',
+            def: 'UNCATEGORIZED'
+          }
+        ],
+        selectedCategory: function() {
+          if (this.get('row')) {
+            return {
+              name: this.get('row.category'),
+              value: this.get('row.category')
+            };
+          } else {
+            return null;
+          }
+        }.property('row', 'row.id', 'row.category', 'rowContent'),
+        selectedCategoryDidChange: function() {
+          if (this.get('row') && this.get('isEditing')) {
+            this.get('column').setCellContent(
+              this.get('row'), this.get('selectedCategory'));
+          }
+        }.observes('selectedCategory'),
+        onRowContentDidChange: function() {
+          this.set('isEditing', false);
+          if (this.get('row')) {
+            console.log('rowContent changed. c: ' + this.get('row.category'));
+            this.set('selectedCategory', {
+              name: this.get('row.category'),
+              value: this.get('row.category')
+            });
+          } else {
+            console.log('rowContent changed. no row.');
+          }
+        }.observes('row', 'row.id', 'row.category'),
+        click: function(event) {
+          this.set('isEditing', true);
+        }
+      })
     });
     var amountColumn = Ember.Table.ColumnDefinition.create({
       isResizable: false,
